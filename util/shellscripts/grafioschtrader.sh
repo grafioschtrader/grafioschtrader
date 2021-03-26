2201b7deab114abc0f652317a16437af4f702c71
Hugo Graf <hg@hugograf.com>
2021-03-26 08:30:08 +0100

    Shell scripts to ease update

---
+++ util/shellscripts/grafioschtrader.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export JASYPT_ENCRYPTOR_PASSWORD=James007BondistVerstorben
+java -Xms528m -Xmx2048m -Duser.language=en -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -jar /home/grafioschtrader/grafioschtrader-server-0.10.0.jar >> /var/log/grafioschtrader.log
+
