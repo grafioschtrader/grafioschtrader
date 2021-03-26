26176675487c4174c2312a099165087400f6ca09
Hugo Graf <hg@hugograf.com>
2021-03-26 16:23:39 +0100

    modify scripts 2

--- util/shellscripts/grafioschtrader.sh
+++ util/shellscripts/grafioschtrader.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 export JASYPT_ENCRYPTOR_PASSWORD=YOUR_Jasypt_PASSWORD
-<<<<<<< HEAD
 java -Xms528m -Xmx2048m -Duser.language=en -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -jar /home/grafioschtrader/grafioschtrader-server-*.jar >> /var/log/grafioschtrader.log
-=======
-java -Xms528m -Xmx2048m -Duser.language=en -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -jar /home/grafioschtrader/grafioschtrader-server-0.10.0.jar >> /var/log/grafioschtrader.log
->>>>>>> 09b85be25b33d4a3d77783417f0e1004836b6b19
