a005610dcdd0c48fae139d69f34faf6ca97f0de7
Hugo Graf <hg@hugograf.com>
2021-04-09 21:24:31 +0200

    some scripts

--- util/shellscripts/gtupdate.sh
+++ util/shellscripts/gtupdate.sh
@@ -4,8 +4,11 @@ sudo systemctl stop grafioschtrader.service
 cd $builddir
 cp grafioschtrader/backend/grafioschtrader-server/src/main/resources/application.properties .
 cd grafioschtrader
-# git reset --hard HEAD
+git reset --hard origin/master
 git pull
+cd ..
+mv grafioschtrader/backend/grafioschtrader-server/src/main/resources/application.properties application.properties.new
+~/merger.sh -i application.properties -s application.properties.new -o grafioschtrader/backend/grafioschtrader-server/src/main/resources/application.properties
 echo Build backend and frontend, only output of frontend is shown
 cd ~
 ~/gtupfrontend.sh &
