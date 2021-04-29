450459789d3cfe9d77b19734ab6200d5a957a932
Hugo Graf <hg@hugograf.com>
2021-04-29 16:29:39 +0200

    Create gtupfrontback.sh

---
+++ util/shellscripts/gtupfrontback.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo Build backend and frontend, only output of frontend is shown
+memorytotal="$(free -m | awk '/^Mem/ { print $2}')"
+cd ~
+if [ $memorytotal -gt 2000 ]
+  then
+    ~/gtupfrontend.sh &
+    ~/gtupbackend.sh &> $builddir/backbuild.log &
+    wait
+    cat $builddir/backbuild.log
+  else
+    sudo systemctl stop grafioschtrader.service
+    ~/gtupfrontend.sh
+    ~/gtupbackend.sh
+  fi
