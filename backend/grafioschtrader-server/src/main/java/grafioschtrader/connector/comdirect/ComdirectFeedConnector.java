690e38f68cae06e04dcf3552b03cd29a585a2d93
Hugo Graf <hg@hugograf.com>
2021-06-03 19:37:42 +0200

    add comdirect for intra

---
+++ backend/grafioschtrader-server/src/main/java/grafioschtrader/connector/comdirect/ComdirectFeedConnector.java
@@ -0,0 +1,81 @@
+package grafioschtrader.connector.comdirect;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import grafioschtrader.common.DataHelper;
+import grafioschtrader.connector.instrument.BaseFeedConnector;
+import grafioschtrader.connector.instrument.FeedConnectorHelper;
+import grafioschtrader.entities.Currencypair;
+import grafioschtrader.entities.Security;
+import grafioschtrader.entities.Securitycurrency;
+
+@Component
+public class ComdirectFeedConnector extends BaseFeedConnector {
+
+  private static Map<FeedSupport, FeedIdentifier[]> supportedFeed;
+  private static String BASE_URL = "https://www.comdirect.de/inf/";
+
+  static {
+    supportedFeed = new HashMap<>();
+    supportedFeed.put(FeedSupport.INTRA,
+        new FeedIdentifier[] { FeedIdentifier.SECURITY_URL, FeedIdentifier.CURRENCY_URL });
+  }
+
+  public ComdirectFeedConnector() {
+    super(supportedFeed, "comdirect", "comdirect", null);
+  }
+
+  @Override
+  public String getSecurityIntradayDownloadLink(final Security security) {
+    return BASE_URL + security.getUrlIntraExtend();
+  }
+  
+  
+  @Override
+  public int getIntradayDelayedSeconds() {
+    return 900;
+  }
+
+  @Override
+  public void updateSecurityLastPrice(final Security security) throws Exception {
+    updateSecuritycurrency(security, getSecurityIntradayDownloadLink(security));
+  }
+
+  @Override
+  public String getCurrencypairIntradayDownloadLink(final Currencypair currencypair) {
+    return BASE_URL + currencypair.getUrlIntraExtend();
+  }
+
+  @Override
+  public void updateCurrencyPairLastPrice(final Currencypair currencypair) throws Exception {
+    updateSecuritycurrency(currencypair, getCurrencypairIntradayDownloadLink(currencypair));
+  }
+
+  private <T extends Securitycurrency<T>> void updateSecuritycurrency(T securitycurrency, String url)
+      throws IOException, ParseException {
+
+    final Connection comdirectConnection = Jsoup.connect(url);
+    final Document doc = comdirectConnection.timeout(10000).get();
+
+    final Element div = doc.select("#keyelement_kurs_update").first();
+    String[] numbers = StringUtils.normalizeSpace(div.text().replace("%", "")).split(" ");
+    securitycurrency.setSLast(FeedConnectorHelper.parseDoubleGE(numbers[0].replaceAll("[A-Z]*$", "")));
+    securitycurrency.setSChangePercentage(FeedConnectorHelper.parseDoubleGE(numbers[1]));
+    securitycurrency
+        .setSOpen(DataHelper.round(securitycurrency.getSLast() - FeedConnectorHelper.parseDoubleGE(numbers[2])));
+    securitycurrency.setSTimestamp(new Date(System.currentTimeMillis() - getIntradayDelayedSeconds() * 1000));
+
+  }
+}
--- backend/grafioschtrader-server/src/test/java/grafioschtrader/connector/instrument/test/ConnectorTestHelper.java
+++ backend/grafioschtrader-server/src/test/java/grafioschtrader/connector/instrument/test/ConnectorTestHelper.java
@@ -11,13 +11,27 @@ public class ConnectorTestHelper {
     return currencyPair;
   }
 
+
+  public static Currencypair createIntraCurrencyPair(final String fromCurrency, final String toCurrency,
+      final String urlIntraExtend) {
+    return createIntraHistoricalCurrencyPair(fromCurrency, toCurrency, urlIntraExtend, null);
+  }
+
+  
   public static Currencypair createHistoricalCurrencyPair(final String fromCurrency, final String toCurrency,
       final String urlUrlHistoryExtend) {
+    return createIntraHistoricalCurrencyPair(fromCurrency, toCurrency, null, urlUrlHistoryExtend);
+  }
+
+  private static Currencypair createIntraHistoricalCurrencyPair(final String fromCurrency, final String toCurrency,
+      String urlIntraExtend, String urlHistoryExtend) {
     final Currencypair currencyPair = new Currencypair();
     currencyPair.setFromCurrency(fromCurrency);
     currencyPair.setToCurrency(toCurrency);
-    currencyPair.setUrlHistoryExtend(urlUrlHistoryExtend);
+    currencyPair.setUrlIntraExtend(urlIntraExtend);
+    currencyPair.setUrlHistoryExtend(urlHistoryExtend);
     return currencyPair;
   }
 
+  
 }
