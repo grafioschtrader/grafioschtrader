1e14f4edd10d033caaafe05c922fa9decbb1eab2
Hugo Graf <hg@hugograf.com>
2021-07-12 20:44:10 +0200

    fix database 0.19.0

--- backend/grafioschtrader-server/src/main/resources/db/migration/V0_19_0__importpostranserror.sql
+++ backend/grafioschtrader-server/src/main/resources/db/migration/V0_19_0__importpostranserror.sql
@@ -1,4 +1,7 @@
-ALTER TABLE transaction DROP CONSTRAINT `s_quotation`;
-ALTER TABLE transaction ADD CONSTRAINT `s_quotation` CHECK (`quotation` is not null and (`quotation` > 0 or `quotation` <> 0 and `transaction_type` BETWEEN 6 AND 7 ) and `id_securitycurrency` is not null or `quotation` is null and `id_securitycurrency` is null);
+# Some Version of mariadb do not work with drop contraint
+#--------------------------------------------------------
+# ALTER TABLE transaction DROP CONSTRAINT `s_quotation`;
+# ALTER TABLE transaction ADD CONSTRAINT `s_quotation` CHECK (`quotation` is not null and (`quotation` > 0 or `quotation` <> 0 and `transaction_type` BETWEEN 6 AND 7 ) and `id_securitycurrency` is not null or `quotation` is null and `id_securitycurrency` is null);
+
 ALTER TABLE `imp_trans_pos` DROP COLUMN IF EXISTS `transaction_error`;
 ALTER TABLE `imp_trans_pos` ADD `transaction_error` VARCHAR(1024) NULL AFTER `known_other_flags`; 
