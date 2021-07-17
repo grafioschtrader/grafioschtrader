67e6474ead07bd6258b9d009d9d2a051e51dc41f
Hugo Graf <hg@hugograf.com>
2021-07-17 15:30:09 +0200

    Update gtupfrontend.sh

--- util/shellscripts/gtupfrontend.sh
+++ util/shellscripts/gtupfrontend.sh
@@ -2,9 +2,14 @@
 . ~/gtvar.sh
 cd $builddir/grafioschtrader/frontend
 npm install
+memorytotal="$(free -m | awk '/^Mem/ { print $2}')"
+if [ $memorytotal -lt 2001 ]
+  then
+    sudo systemctl stop grafioschtrader.service
+    NODE_OPTIONS=--max_old_space_size=1400
+  fi
 ng build --prod --base-href /$basehref --deploy-url /$basehref
 rm -r $docroot/${basehref}assets
 rm $docroot/${basehref}*
 cp -r $builddir/grafioschtrader/frontend/dist/* $docroot/$basehref
  
-
