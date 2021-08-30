832e1d45400b4d01de9ee857f2a0b6b2e7b6b350
Hugo Graf <hg@hugograf.com>
2021-08-30 10:34:06 +0200

    Update checkversion.sh

--- util/shellscripts/checkversion.sh
+++ util/shellscripts/checkversion.sh
@@ -1,6 +1,8 @@
 CLIVERSION=`npm list -global --depth 0 | grep "@angular/cli" | cut -d "@" -f3 | cut -d "." -f1`
 if [[ -z "$CLIVERSION" ||  "$CLIVERSION" -lt 12 ]]
   then
+    rm -f $builddir/grafioschtrader/frontend/node_modules
+    rm -f $builddir/grafioschtrader/frontend/package-lock.json
     tput setaf 1
     echo ==========================================================
     echo "Run as root"
