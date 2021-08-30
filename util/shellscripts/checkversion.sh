f19589f939ea53f71c690ce9a09635eeabd83927
Hugo Graf <hg@hugograf.com>
2021-08-30 09:37:45 +0200

    Create checkversion.sh

---
+++ util/shellscripts/checkversion.sh
@@ -0,0 +1,13 @@
+CLIVERSION=`npm list -global --depth 0 | grep "@angular/cli" | cut -d "@" -f3 | cut -d "." -f1`
+if [[ -z "$CLIVERSION" ||  "$CLIVERSION" -lt 12 ]]
+  then
+    tput setaf 1
+    echo ==========================================================
+    echo "Run as root"
+    echo "  npm uninstall -g @angular/cli"
+    echo "  npm install -g @angular/cli@^12"
+    echo ==========================================================
+    tput sgr 0
+    exit 1
+  fi
+exit 0
