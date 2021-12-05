44b55e0f1d35274e4880d40baa0e38347a44ca54
Hugo Graf <hg@hugograf.com>
2021-12-05 17:09:00 +0100

    Update gtupfrontend.sh

--- util/shellscripts/gtupfrontend.sh
+++ util/shellscripts/gtupfrontend.sh
@@ -6,9 +6,9 @@ memorytotal="$(free -m | awk '/^Mem|Speicher/ { print $2}')"
 if [ $memorytotal -lt 2001 ]
   then
     sudo systemctl stop grafioschtrader.service
-    export NODE_OPTIONS="--max_old_space_size=1600"
+    export NODE_OPTIONS="--max_old_space_size=1900"
   fi
-ng build --configuration production --base-href /$basehref --deploy-url /$basehref
+ng build --configuration production --base-href /$basehref
 rm -r $docroot/${basehref}assets
 rm $docroot/${basehref}*
 cp -r $builddir/grafioschtrader/frontend/dist/* $docroot/$basehref
