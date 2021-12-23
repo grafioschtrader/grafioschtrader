15ba0b62789acb7c8783d21bbb038314c14f69a8
Hugo Graf <hg@hugograf.com>
2021-12-23 17:18:18 +0100

    Update grafioschtrader.sh

--- util/shellscripts/grafioschtrader.sh
+++ util/shellscripts/grafioschtrader.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 export JASYPT_ENCRYPTOR_PASSWORD=YOUR_Jasypt_PASSWORD
-java -Xms528m -Xmx2048m -Duser.language=en -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 -jar /home/grafioschtrader/grafioschtrader-server-*.jar >> /var/log/grafioschtrader.log
+java -Xms528m -Xmx2048m -Duser.language=en -jar /home/grafioschtrader/grafioschtrader-server-*.jar >> /var/log/grafioschtrader.log
