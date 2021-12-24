1cc1a69b48f65a52d9c66c3650511a9a85ab7257
Hugo Graf <hg@hugograf.com>
2021-12-24 11:21:46 +0100

    Update gtupfrontback.sh

--- util/shellscripts/gtupfrontback.sh
+++ util/shellscripts/gtupfrontback.sh
@@ -3,7 +3,7 @@ echo Build backend and frontend, only output of frontend is shown
 memorytotal="$(free -m | awk '/^Mem|Speicher/ { print $2}')"
 . ~/gtvar.sh
 cd ~
-if [ $memorytotal -gt 2000 ]
+if [ $memorytotal -gt 4000 ]
   then
     ~/gtupfrontend.sh &
     ~/gtupbackend.sh &> $builddir/backbuild.log &
