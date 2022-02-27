f74cd279763e6ee8bb340cb5eaa77b94d033ae7d
Hugo Graf <hg@hugograf.com>
2022-02-27 14:34:10 +0100

    Update gtupfrontend.sh

--- util/shellscripts/gtupfrontend.sh
+++ util/shellscripts/gtupfrontend.sh
@@ -3,13 +3,19 @@
 cd $builddir/grafioschtrader/frontend
 npm install
 memorytotal="$(free -m | awk '/^Mem|Speicher/ { print $2}')"
-if [ $memorytotal -lt 4001 ]
+if (($memorytotal>=4000 && memorytotal<=4048))
   then
     sudo systemctl stop grafioschtrader.service
     export NODE_OPTIONS="--max_old_space_size=4071"
   fi
-ng build --configuration production --base-href /$basehref
+if [ $memorytotal -lt 4000 ]
+   then
+    cd $builddir/grafioschtrader/frontend
+    wget https://github.com/grafioschtrader/grafioschtrader/releases/download/Latest/latest.tar.gz
+    tar -xf latest.tar.gz
+    cd
+   else
+    ng build --configuration production --base-href /$basehref
+   fi
 rm -r $docroot/${basehref}assets
 rm $docroot/${basehref}*
-cp -r $builddir/grafioschtrader/frontend/dist/* $docroot/$basehref
- 
