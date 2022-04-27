296e64464aa748f8e95a98bb086751631280c2c8
Hugo Graf <hg@hugograf.com>
2022-04-27 08:36:56 +0200

    Update gtupfrontback.sh

--- util/shellscripts/gtupfrontback.sh
+++ util/shellscripts/gtupfrontback.sh
@@ -4,7 +4,7 @@ memorytotal="$(free -m | awk '/^Mem|Speicher/ { print $2}')"
 . ~/gtvar.sh
 rm -rf $builddir/.deps
 cd ~
-if [ $memorytotal -gt 6000 ]
+if [ $memorytotal -gt 4000 ]
   then
     ~/gtupfrontend.sh &
     ~/gtupbackend.sh &> $builddir/backbuild.log &
