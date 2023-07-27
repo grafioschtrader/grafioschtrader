a9a81eb851d2f344924062b3b651dcbd6f180078
hugograf <hg@hugograf.com>
2023-07-27 15:14:10 +0200

    Maybe primeNG 16.1.0 has implemented new bugs. Stay with 16.0.2

--- backend/grafioschtrader-server/src/main/java/grafioschtrader/repository/GTNetMessageAnswerJpaRepositoryImpl.java
+++ backend/grafioschtrader-server/src/main/java/grafioschtrader/repository/GTNetMessageAnswerJpaRepositoryImpl.java
@@ -1,21 +1,31 @@
 package grafioschtrader.repository;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import grafioschtrader.entities.GTNet;
 import grafioschtrader.entities.GTNetMessage;
 import grafioschtrader.entities.GTNetMessageAnswer;
+import grafioschtrader.gtnet.GTNetMessageCodeType;
 import grafioschtrader.gtnet.m2m.model.MessageEnvelope;
 
 public class GTNetMessageAnswerJpaRepositoryImpl extends BaseRepositoryImpl<GTNetMessageAnswer>
- implements GTNetMessageAnswerJpaRepositoryCustom {
+    implements GTNetMessageAnswerJpaRepositoryCustom {
 
   @Autowired
   private GTNetMessageAnswerJpaRepository gtNetMessageAnswerJpaRepository;
 
   @Override
   public GTNetMessage getMessageAnswerBy(GTNet myGTNet, GTNet remoteGTNet, MessageEnvelope meRequest) {
-    // TODO Auto-generated method stub
-    return null;
+    GTNetMessage gtNetMessageAnw = new GTNetMessage();
+    Optional<GTNetMessageAnswer> messageAnswerOpt = gtNetMessageAnswerJpaRepository.findById(meRequest.messageCode);
+    if(messageAnswerOpt.isEmpty()) {
+      if(meRequest.messageCode == GTNetMessageCodeType.GT_NET_FIRST_HANDSHAKE_S.getValue()) {
+        
+      }
+    }
+    
+    return gtNetMessageAnw;
   }
 }
