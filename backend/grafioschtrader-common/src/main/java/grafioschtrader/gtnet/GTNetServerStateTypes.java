6a1f1bd863b5f4177a435fb7cb03c16175e1be8f
hugograf <hg@hugograf.com>
2023-07-27 15:14:10 +0200

    Maybe primeNG 16.1.0 has implemented new bugs. Stay with 16.0.2

--- backend/grafioschtrader-common/src/main/java/grafioschtrader/gtnet/GTNetServerStateTypes.java
+++ backend/grafioschtrader-common/src/main/java/grafioschtrader/gtnet/GTNetServerStateTypes.java
@@ -4,12 +4,14 @@ public enum GTNetServerStateTypes {
 
   // There is no support for last price or entity
   SS_NONE((byte) 0), 
+  // First handshake failed, cannot repeated
+  SS_FAILED_HANDSHAKE ((byte) 1),
   // There is support for last price or entity, but no more remote domain is allowed
-  SS_CLOSED((byte) 1), 
+  SS_CLOSED((byte) 2), 
   // The system is in maintenance but will come back
-  SS_MAINTENANCE((byte) 2), 
+  SS_MAINTENANCE((byte) 3), 
   // There is support for last price or entity
-  SS_OPEN((byte) 3);
+  SS_OPEN((byte) 4);
 
   private final Byte value;
 
