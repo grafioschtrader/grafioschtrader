5de02aba75ac3785695340c312498bae363d3327
hugograf <hg@hugograf.com>
2024-03-18 16:01:50 +0100

    Distinct between FS_HISTORY and FS_INTRA

--- backend/grafioschtrader-server/src/main/java/grafioschtrader/task/exec/MonitorIntradayPriceDataTask.java
+++ backend/grafioschtrader-server/src/main/java/grafioschtrader/task/exec/MonitorIntradayPriceDataTask.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import grafioschtrader.GlobalConstants;
 import grafioschtrader.alert.AlertType;
+import grafioschtrader.connector.instrument.IFeedConnector.FeedSupport;
 import grafioschtrader.entities.TaskDataChange;
 import grafioschtrader.exceptions.TaskBackgroundException;
 import grafioschtrader.repository.SecurityJpaRepository.MonitorFailedConnector;
@@ -16,10 +17,10 @@ import grafioschtrader.types.TaskDataExecPriority;
 import grafioschtrader.types.TaskType;
 
 /**
- * Checks whether a connector for intraday price data may no longer be
- * working. Certain parameters of the check can be adjusted via the global
- * settings. In the event of a possible malfunction of a connector, the main
- * administrator receives a message. This job should be carried out daily.
+ * Checks whether a connector for intraday price data may no longer be working.
+ * Certain parameters of the check can be adjusted via the global settings. In
+ * the event of a possible malfunction of a connector, the main administrator
+ * receives a message. This job should be carried out daily.
  */
 @Component
 public class MonitorIntradayPriceDataTask extends MonitorPriceData implements ITask {
@@ -29,12 +30,12 @@ public class MonitorIntradayPriceDataTask extends MonitorPriceData implements IT
     TaskDataChange taskDataChange = new TaskDataChange(getTaskType(), TaskDataExecPriority.PRIO_NORMAL);
     taskDataChangeRepository.save(taskDataChange);
   }
-  
+
   @Override
   public TaskType getTaskType() {
     return TaskType.MONITOR_INTRADAY_PRICE_DATA;
   }
-  
+
   @Override
   public void doWork(TaskDataChange taskDataChange) throws TaskBackgroundException {
     List<MonitorFailedConnector> monitorFailedConnectors = securityJpaRepository.getFailedIntradayConnector(
@@ -43,7 +44,8 @@ public class MonitorIntradayPriceDataTask extends MonitorPriceData implements IT
             - globalparametersJpaRepository.getIntradayObservationRetryMinus(),
         globalparametersJpaRepository.getIntradayObservationFallingPercentage());
     if (!monitorFailedConnectors.isEmpty()) {
-      generateMessageAndPublishAlert(AlertType.ALERT_CONNECTOR_INTRADAY_MAY_NOT_WORK_ANYMORE, monitorFailedConnectors);
+      generateMessageAndPublishAlert(AlertType.ALERT_CONNECTOR_INTRADAY_MAY_NOT_WORK_ANYMORE, monitorFailedConnectors,
+          FeedSupport.FS_INTRA);
     }
   }
 }
