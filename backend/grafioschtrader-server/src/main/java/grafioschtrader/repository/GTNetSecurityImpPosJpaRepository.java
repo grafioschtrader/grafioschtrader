100b6b2dc12913fdd9abf5f94646cf93547e86ae
hugograf <hg@hugograf.com>
2026-02-05 11:41:46 +0100

    Continue on #82

--- backend/grafioschtrader-server/src/main/java/grafioschtrader/repository/GTNetSecurityImpPosJpaRepository.java
+++ backend/grafioschtrader-server/src/main/java/grafioschtrader/repository/GTNetSecurityImpPosJpaRepository.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import grafioschtrader.entities.GTNetSecurityImpPos;
